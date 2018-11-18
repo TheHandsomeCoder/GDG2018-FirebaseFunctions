046121b241ccab35ba94cfd1cd7c1c4f675c71b3
Scott O'Malley <scottyomalley@gmail.com>
2018-11-18 14:03:49 +0000

    Storing the Phone Number is working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,24 @@
 import * as functions from 'firebase-functions';
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+    // Grab the text parameter.
+    if (!request.body.From){
+        response.status(500).send('Missing Required Data');
+    }
+
+    const phoneNumber: string = request.body.From;
+    const lastCalled = new Date();
+
+    admin.firestore().collection('lonelyPeople')
+        .add({ phoneNumber, lastCalled })
+        .then(result => {
+            response.send("Successfully Added Person")
+        }).catch(error => {
+            response.status(500).send('Something bad happened');
+        });
+});
